833c93035b26e910ca38092f48c2ec3b78f4a8f4
Varun Patel <rvjpatel99@gmail.com>
2025-03-12 22:23:45 -0700

    setup updated score ui
    now working on the scripts for score ui

---
+++ Assets/_Scripts/Logic/CoinCounterUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField]
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
